ea8f8132245dc0e59bb865e6c3e4cf58f19e9422
MrThief123 <ishan.d.mail@gmail.com>
2024-07-14 14:04:33 +1000

    simplify

--- index.js
+++ index.js
@@ -1,54 +1,13 @@
 document.addEventListener('keydown', function(event) {
     switch (event.key) {
-        case "1":
-            display.value += '1';
-          break;
-        case "3":
-            display.value += '3';
-        break;
-        case "2":
-            display.value += '2';
-        break;
-        case "4":
-            display.value += '4';
-        break;
-        case "5":
-            display.value += '5';
-        break;
-        case "6":
-            display.value += '6';
-        break;
-        case "7":
-            display.value += '7';
-        break;
-        case "8":
-            display.value += '8';
-        break;
-        case "9":
-            display.value += '9';
-        break;
-        case "0":
-            display.value += '0';
-        break;
-        case "+":
-            display.value += '+';
-        break;
-        case "-":
-            display.value += '-';
-        break;
-        case "/":
-            display.value += '/';
-        break;
-        case "*":
-            display.value += '*';
-        break;
         case "Enter":
             calculate();
-        break;
-        
-
+            break;
         default:
-          return; // Quit when this doesn't handle the key event.
+            if ("0123456789+-/*".includes(event.key)) {
+                display.value += event.key;
+            }
+            break;
 }});
 
 const display = document.getElementById("display");
